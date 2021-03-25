b149ee5a793360677b1e64286dd81b6afc72d2bf
shibacrypt <>
2021-03-25 00:00:00 +0000

    Add snow farm base

---
+++ contracts/ISnowballLandFarm.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.12;
+
+interface ISnowballLandFarm {
+    function poolLength() external view returns (uint256);
+
+    function addPool(
+        uint256 _allocPoint,
+        address _stakeToken,
+        bool _withUpdate
+    ) external;
+
+    function setPool(
+        uint256 _pid,
+        uint256 _allocPoint,
+        bool _withUpdate
+    ) external;
+
+    function pendingSnow(uint256 _pid, address _user) external view returns (uint256);
+
+    function updatePool(uint256 _pid) external;
+
+    function deposit(address _for, uint256 _pid, uint256 _amount) external;
+
+    function withdraw(address _for, uint256 _pid, uint256 _amount) external;
+
+    function withdrawAll(address _for, uint256 _pid) external;
+
+    function harvest(uint256 _pid) external;
+}
